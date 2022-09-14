525f5fc317da6caeb8d265790f15e98ec3a7dec0
Effing Crow <effingcrow@gmail.com>
2022-09-14 14:44:18 +0300

    fix

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -36,11 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={
-          item.image === "" || item.image === null || !item.image
-            ? "placeholder.png"
-            : item.image
-        }
+        src={!item.image ? "placeholder.png" : item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
